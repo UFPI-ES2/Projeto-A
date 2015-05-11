a3a7d580a814b1e0724938467a825aec6e76b3d5
Tiago Patrocinio <tiagodsp93@gmail.com>
2015-05-11 18:28:36 -0300

    Some updates

--- ocrSys/views.py
+++ ocrSys/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 
-def scanDocument(request):
+
+def scanDocumentForm(request):
     template = 'scan_document.html'
     context = {}
     return render_to_response(template, context)

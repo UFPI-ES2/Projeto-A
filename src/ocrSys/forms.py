7dbe409ec3f1218fe6b7e5d3a5da2b2b0f0e3184
Tiago Patrocinio <tiagodsp93@gmail.com>
2015-06-07 21:50:47 -0300

    Atualizando CSS e correção de Bugs

--- src/ocrSys/forms.py
+++ src/ocrSys/forms.py
@@ -4,3 +4,6 @@ class UploadImageToOCR(forms.Form):
     name = forms.CharField(max_length=50)
     file = forms.FileField()
 #     bool = forms.BooleanField
+
+class DocumentForm(forms.Form):
+    pass
